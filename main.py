b12912e632d43e4a09611aa62fdf647464826d11
extectick <extectick@yandex.ru>
2021-11-15 10:10:39 +0700

    'search'

---
+++ main.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Firefox(executable_path='geckodriver.exe')
+driver.get("http://www.ya.ru")
+
+time.sleep(2)
+elem = driver.find_element_by_id("text")
+elem.clear()
+elem.send_keys("котики картинки")
+elem.send_keys(Keys.RETURN)
+time.sleep(5)
+driver.close()
